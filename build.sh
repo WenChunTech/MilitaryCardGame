e45ab5a4af47f074417ae82bf2e83dad64886353
fwc <1643806656@qq.com>
2024-11-19 00:05:19 +0800

    build(bun): add build for MilitaryCardGameFrontend

--- build.sh
+++ build.sh
@@ -1,8 +1,16 @@
 #!/usr/bin/env bash
 
+cur=$(pwd)
+
+# build for MilitaryCardGameFrontend
+cd MilitaryCardGameFrontend
+bun run install
+bun run build
+
+# build for MilitaryCardGame
+cd $cur
 GOOS=(darwin linux windows)
 GOARCH=(amd64 arm64)
-
 for os in ${GOOS[@]}; do
     for arch in ${GOARCH[@]}; do
         echo "Building for $os $arch"
